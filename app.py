36f7ce6ca6c5e28487bbe5f86f2c73a31cbdbd09
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-04 00:48:57 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+
+# Agrega una caja de texto para que el usuario ingrese el tema del ensayo
+topic = st.text_input("Ingrese el tema del ensayo:")
+
+# Genera los títulos de los cinco subensayos
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt="Por favor, genera cinco títulos de subensayos para un ensayo sobre " + topic + ".",
+    max_tokens=1024
+)
+subtopic_titles = response["choices"][0]["text"].split("\n")
+
+# Muestra los títulos al usuario y permite que elija los subtítulos que desee utilizar
+selected_subtopics = st.multiselect("Seleccione los subtítulos para incluir en el ensayo:", subtopic_titles)
+
+# Genera un subensayo para cada subtítulo seleccionado
+subessays = {}
+for subtopic in selected_subtopics:
+  response = openai.Completion.create(
+      engine="text-davinci-003",
+      prompt="Por favor, genera un subensayo sobre " + subtopic + ".",
+      max_tokens=1024
+  )
+  subessay = response["choices"][0]["text"]
+  subessays[subtopic] = subessay
+
+# Muestra el contenido de subessays
+print(subessays)
+
+# Muestra el ensayo completo al usuario
+st.write("Ensayo completo:")
+full_essay = ""
+for subtopic, subessay in subessays.items():
+  full_essay += "**" + subtopic + "**\n" + subessay + "\n\n"
+st.markdown(full_essay)
